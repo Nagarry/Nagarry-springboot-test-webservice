2c1467b2bdb2efcc83e968a0cffaeeef37c8bbdd
nagarry <lji0828@gmail.com>
2021-03-14 10:32:29 +0900

    Application등록, 단위테스트

---
+++ src/main/java/me/synology/nagarry/Application.java
@@ -0,0 +1,11 @@
+package me.synology.nagarry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
